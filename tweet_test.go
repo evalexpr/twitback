f3ddd2cf5f58b81abe2d2d7d8a753460180514b6
W1lkins <wilkinsphysics@gmail.com>
2019-01-13 20:06:41 +0000

    add tweet_test

---
+++ tweet_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/dghubble/go-twitter/twitter"
+)
+
+func TestHandleEntities(t *testing.T) {
+	type fields struct {
+		Tweet twitter.Tweet
+	}
+	type args struct {
+		dir string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		shouldErr bool
+	}{
+		{
+			"No media returns nil",
+			fields{twitter.Tweet{ExtendedEntities: &twitter.ExtendedEntity{Media: nil}}},
+			args{"foo"},
+			false,
+		},
+		{
+			"Photo downloads the photo",
+			fields{
+				twitter.Tweet{
+					ExtendedEntities: &twitter.ExtendedEntity{
+						Media: []twitter.MediaEntity{
+							{Type: "photo", MediaURL: "http://test.wilkins.tech/twitback/test.png"},
+						},
+					},
+				},
+			},
+			args{"test_fixtures"},
+			false,
+		},
+		{
+			"Video downloads the video",
+			fields{
+				twitter.Tweet{
+					ExtendedEntities: &twitter.ExtendedEntity{
+						Media: []twitter.MediaEntity{
+							{
+								Type: "video",
+								VideoInfo: twitter.VideoInfo{
+									Variants: []twitter.VideoVariant{{URL: "http://test.wilkins.tech/twitback/test.mp4"}},
+								},
+							},
+						},
+					},
+				},
+			},
+			args{"test_fixtures"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tw := &tweet{
+				Tweet: tt.fields.Tweet,
+			}
+			if err := tw.HandleEntities(tt.args.dir); (err != nil) != tt.shouldErr {
+				t.Errorf("tweet.HandleEntities() failed, error: %v, shouldErr: %v", err, tt.shouldErr)
+			}
+		})
+	}
+}
